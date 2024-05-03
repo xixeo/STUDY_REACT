c187213d983ddb1ff3395afa0c3c13e44ea51f2c
xixeo <rud3419@naver.com>
2024-05-03 16:33:06 +0900

    module.css 사용법

--- src/02/MyClockTime.js
+++ src/02/MyClockTime.js
@@ -1,9 +1,13 @@
 import './MyClockStyle.css'
+import style from './My.module.css'
 
 function MyClockTime(){
   const now = new Date();
   const nowStr = now.toLocaleTimeString();
   const gubun = nowStr.substring(0, 2);
+  // const st ={
+  //   color: "yellow", fontWeight: "bold", fontSize: "1.2rem"
+  // }
   // let divStyle;
   // if(gubun == '오전') divStyle = "div1";
   // else divStyle = "div2";
@@ -15,7 +19,11 @@ function MyClockTime(){
                       :  <div  className='div2'>{nowStr}</div>
     }    */}
 
-    <div className={(gubun === '오전') ? 'div1':'div2'}>{nowStr}</div>
+    {/* <div className={(gubun === '오전') ? 'div1':'div2'}>{nowStr}</div> */}
+
+    {/* <div style={{color: "yellow", fontWeight: "bold", fontSize: "1.2rem"}}>{nowStr}</div> */}
+    {/* <div style={st}>{nowStr}</div> */}
+    <div className={style.c1}>{nowStr}</div>
     </>
   )
 }
