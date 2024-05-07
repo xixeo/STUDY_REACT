2f9b94905a20e21579eab4bf1e973aa3549b2725
xixeo <rud3419@naver.com>
2024-05-07 16:27:07 +0900

    useState로 클릭이벤트 적용

--- src/03/MyDiv.js
+++ src/03/MyDiv.js
@@ -1,14 +1,29 @@
 import MyDiv2 from "./MyDiv2";
+import { BsFillSuitHeartFill } from "react-icons/bs";
+//1. useState import
+import { useState } from "react";
 
 export default function MyDiv() {
+    //2. state변수 선언
+    const[n, setN] = useState(0) //n의 초기값 0 선언;
+
+    //3. setN을 통해서 n의 값 변경
+    const handleCount = () => {
+        setN(n + 1);
+    }
+
     const dName1 = "div 1";
     const dName2 = "div 2";
     const dName3 = "div 3";
 
+
     return (
-        <div className="w-1/2 h-1/2 bg-emerald-300 text-white text-md px-8 py-5 items-center flex flex-col justify-center">
-            {dName1}
+        <div className="w-1/2 h-1/2 border border-stone-300 text-white text-md px-8 py-5 items-center flex flex-col justify-center">
             <MyDiv2 dn1={dName1} dn2={dName2} dn3={dName3} />
+            <div className="w-full flex justify-end itmes-center mt-3">
+                <span onClick={handleCount} className="text-red-500 text-xl cursor-pointer"><BsFillSuitHeartFill /></span>
+                <span className="ml-2 text-stone-900">{n}</span>
+            </div>
         </div>
     );
 }
--- src/03/MyDiv2.js
+++ src/03/MyDiv2.js
@@ -2,7 +2,7 @@ import MyDiv3 from "./MyDiv3";
 
 export default function MyDiv2({ dn1, dn2, dn3 }) {
     return (
-        <div className=" w-1/2 h-1/2 text-emerald-600  bg-emerald-100 justify-center flex flex-col items-center">
+        <div className="m-3 w-full h-full text-emerald-600  bg-rose-200 justify-center flex flex-col items-center">
             {`${dn1} > ${dn2}`}
             <MyDiv3 dn1={dn1} dn2={dn2} dn3={dn3} />
         </div>
